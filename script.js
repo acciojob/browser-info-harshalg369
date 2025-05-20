67f122c7be184e706fc387a5c05557cfcde48a88
harshalg369 <hxrsh369@gmail.com>
2025-05-21 01:30:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Function to detect browser name and version from navigator object
 function getBrowserInfo() {
     const userAgent = navigator.userAgent;
     const appName = navigator.appName;
@@ -7,12 +6,10 @@ function getBrowserInfo() {
     let browserName = appName;
     let version = appVersion;
 
-    // More accurate browser detection using userAgent string
     if (userAgent.indexOf("Firefox") > -1) {
         browserName = "Mozilla Firefox";
         version = userAgent.match(/Firefox\/([0-9\.]+)/)[1];
     } else if (userAgent.indexOf("Chrome") > -1) {
-        // Check if it's Edge Chromium first
         if(userAgent.indexOf("Edg") > -1) {
             browserName = "Microsoft Edge";
             version = userAgent.match(/Edg\/([0-9\.]+)/)[1];
@@ -20,7 +17,7 @@ function getBrowserInfo() {
             browserName = "Google Chrome";
             version = userAgent.match(/Chrome\/([0-9\.]+)/)[1];
         }
-    } else if (userAgent.indexOf("Safari") > -1) {
+    } else if (userAgent.indexOf("Safari") > -1 && userAgent.indexOf("Chrome") === -1) {
         browserName = "Safari";
         version = userAgent.match(/Version\/([0-9\.]+)/)[1];
     } else if (userAgent.indexOf("MSIE") > -1 || userAgent.indexOf("Trident") > -1) {
@@ -43,5 +40,4 @@ function displayBrowserInfo() {
     infoDiv.textContent = `You are using ${browserName} version ${version}`;
 }
 
-// Run on page load
 window.onload = displayBrowserInfo;
